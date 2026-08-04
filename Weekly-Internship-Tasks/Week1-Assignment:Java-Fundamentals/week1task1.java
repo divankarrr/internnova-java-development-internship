fbfff2502c40689dded8ee3754e6f5f92f579222
DIVANKAR SINGH <divankarsingh17@gmail.com>
2026-08-04 14:25:57 +0530

    Add files via upload

---
+++ Weekly-Internship-Tasks/Week1-Assignment:Java-Fundamentals/week1task1.java
@@ -0,0 +1,16 @@
+/*Task 1: Java Setup & Hello World (10 Marks)
+
+Create your first Java program.
+The program should display:
+Your Name
+College Name
+Branch
+A Welcome Message */
+public class week1task1 {
+    public static void main(String[] args) {
+        System.out.println("My Name :DIVANKAR SINGH");
+        System.out.println("College Name : KIET GROUP OF INSTITUTIONS");
+        System.out.println("Branch : COMPUTER SCIENCE AND ENGINEERING");
+        System.out.println("WELCOME TO MY WEEKLY TASKS");
+    }
+}
